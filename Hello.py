c6f10dbb770f973f6c7c5ee5c002b25eb9db9eb0
CtrlF4 <leew99@googlemail.com>
2018-07-10 22:21:44 +0100

    Added reading input values and the length command to the beginner file

--- Hello.py
+++ Hello.py
@@ -1,2 +1,29 @@
 #this is how comments are written
-print("Hello")
+print("Hello World!")
+
+#Here are some basic Maths
+
+#Variables
+add = 2+2
+sub = 4-2
+div = 4 / 2
+mul = 2*2
+first = "John"
+second = 'Smith'
+
+#Using variables in strings
+print("2 + 2 = ", add)
+print("2 - 2 = ", sub)
+print("4 / 2 = ", div)
+print("2 * 2 = ", mul)
+
+#String Concat
+print(first + ' ' + second)
+
+#reading input
+print("Hi! Please Enter Your name:")
+name = input()
+
+#introduction of the len command
+print("Nice to meet you ",name)
+print("You have ", len(name), "letters in your name")

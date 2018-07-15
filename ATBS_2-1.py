f9c2f9cdb2b2abea815d3f0791d22b708ae7e550
CtrlF4 <leew99@googlemail.com>
2018-07-15 13:19:22 +0100

    Addition of first conditional practice

---
+++ ATBS_2-1.py
@@ -0,0 +1,20 @@
+#basic practice of conditionals and booleans
+# indentation delimits blocks, blocks dont end until the indentation ends
+
+print("Please enter your name")
+name = input()
+
+if name == "Lee":
+    print("Welcome Back Lee")
+else:
+    print("Hello", name)
+
+print("Please enter the password")
+password = input()
+
+if password == "Swordfish":
+    print("Access granted")
+else:
+    print("Permission denied")
+
+
